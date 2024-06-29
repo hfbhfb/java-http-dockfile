42e1ec2ebc66b3e82994f0cfa0a6533f0819d571
hfbhfb <hefabao@126.com>
2024-06-29 17:46:44 +0800

    更新buildrunjava.sh，当业务监听端口时，热更新先杀进程。

--- for-http-dockerfile/src/main/java/com/hfbhfb/App.java
+++ for-http-dockerfile/src/main/java/com/hfbhfb/App.java
@@ -1,14 +1,32 @@
 package com.hfbhfb;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("Server is listening on port 8080");
+    }
+
+    static class MyHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello, World! 888  ";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
